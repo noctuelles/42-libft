1f4e7677c0bb59d459c66eb02aa4ed691e12550a
noctuelles <plouvel@student.42.fr>
2022-02-28 14:46:02 +0100

    Semantic update, added ft_strdelchr

--- srcs/string/ft_strdelchrs.c
+++ srcs/string/ft_strdelchrs.c
@@ -1,26 +1,47 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strdelch.c                                      :+:      :+:    :+:   */
+/*   ft_strdelchr.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: plouvel <plouvel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/02/28 14:17:09 by plouvel           #+#    #+#             */
-/*   Updated: 2022/02/28 14:41:42 by plouvel          ###   ########.fr       */
+/*   Created: 2022/02/23 11:46:02 by plouvel           #+#    #+#             */
+/*   Updated: 2022/02/23 12:20:51 by plouvel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h>
+#include <stddef.h>
 
-char	*ft_strdelch(char *str)
+static inline void	shift_str(char *str, unsigned char c)
 {
 	size_t	i;
 
 	i = 0;
 	while (str[i] != '\0')
 	{
+		if (str[i + 1] == c)
+			shift_str(&str[i + 1], c);
 		str[i] = str[i + 1];
 		i++;
+	}
+}
+
+char	*ft_strdelchr(char *str, const char *charset)
+{
+	size_t	i;
+	size_t	j;
+
+	j = 0;
+	while (charset[j] != '\0')
+	{
+		i = 0;
+		while (str[i] != '\0')
+		{
+			if (str[i] == (unsigned char) charset[j])
+				shift_str(&str[i], (unsigned char) charset[j]);
+			i++;
+		}
+		j++;
 	}
 	return (str);
 }
